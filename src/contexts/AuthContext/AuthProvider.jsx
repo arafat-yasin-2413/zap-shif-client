3385fd84f37b177671f4e1f7584d7c7e8aa089a4
Yasin <yasinarafat2413@gmail.com>
2025-06-23 12:02:40 +0600

    zap-shift createUser function done

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
+import { auth } from '../../firebase/firebase.init';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const AuthProvider = ({children}) => {
 
+    const createUser  = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
 
     const authInfo = {
+        createUser,
+
 
     }
 
