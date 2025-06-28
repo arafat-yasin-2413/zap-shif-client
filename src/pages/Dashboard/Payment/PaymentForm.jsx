906664b29571069df0d47f80700c9c4d2ef32b67
Yasin <yasinarafat2413@gmail.com>
2025-06-28 12:40:46 +0600

    66-3 Done

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,36 +1,57 @@
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
-
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
 
 const PaymentForm = () => {
-
     const stripe = useStripe();
     const elements = useElements();
-    
-    const handleSubmit = (e) => {
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return;
         }
 
         const card = elements.getElement(CardElement);
 
-        if(!card) {
+        if (!card) {
             return;
         }
-    }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: "card",
+            card,
+        });
+
+        if (error) {
+            setError(error.message);
+            console.log("[error]", error);
+        } else {
+            setError('');
+            console.log("[PaymentMethod]", paymentMethod);
+        }
+    };
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <CardElement>
-                    <button type='submit' disabled={!stripe}>
+            <form
+                onSubmit={handleSubmit}
+                className="space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto"
+            >
+                <CardElement className="p-2 border rounded">
+                </CardElement>
+                    <button 
+                    type="submit"
+                    className="btn btn-primary text-black w-full" 
+                    disabled={!stripe}>
                         Pay For Parcel Pickup
                     </button>
-                </CardElement>
 
 
+                    {
+                        error && <p className="text-red-500">{error}</p>
+                    }
             </form>
         </div>
     );
