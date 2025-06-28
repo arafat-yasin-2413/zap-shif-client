ff3833240a30de7d5671eaaea03840c8238cc4a7
Yasin <yasinarafat2413@gmail.com>
2025-06-28 23:07:12 +0600

    66-6 Done

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -3,34 +3,34 @@ import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 import { useParams } from "react-router";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
 
 const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
-    const {parcelId} = useParams();
-    const [error, setError] = useState('');
+    const { parcelId } = useParams();
+    const { user } = useAuth();
+    const [error, setError] = useState("");
     const axiosSecure = useAxiosSecure();
 
     // console.log(parcelId);
 
-    const { isPending, data: parcelInfo= {} } = useQuery({
-        queryKey: ['parcels', parcelId],
-        queryFn: async()=>{
+    const { isPending, data: parcelInfo = {} } = useQuery({
+        queryKey: ["parcels", parcelId],
+        queryFn: async () => {
             const res = await axiosSecure.get(`/parcels/${parcelId}`);
             return res.data;
-        }
-    })
+        },
+    });
 
-    if(isPending) {
-        return '... loading'
+    if (isPending) {
+        return "... loading";
     }
-
 
     console.log(parcelInfo);
     const amount = parcelInfo.cost;
-    const amountInCents = amount*100;
+    const amountInCents = amount * 100;
     // console.log('amount in cents : ', amountInCents, typeof amountInCents);
-
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -45,6 +45,7 @@ const PaymentForm = () => {
             return;
         }
 
+        // step-1 -->  validate the card
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: "card",
             card,
@@ -54,36 +55,39 @@ const PaymentForm = () => {
             setError(error.message);
             console.log("[error]", error);
         } else {
-            setError('');
+            setError("");
             console.log("[PaymentMethod]", paymentMethod);
-        }
+            // step:2 --> create payment intent
+            const res = await axiosSecure.post("/create-payment-intent", {
+                amountInCents,
+                parcelId,
+            });
 
-        // step:2 --> create payment intent
-        const res = await axiosSecure.post('/create-payment-intent',{
-            amountInCents,
-            parcelId
-        })
+            const clientSecret = res.data.clientSecret;
 
-        const clientSecret = res.data.clientSecret;
-        const result = await stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: elements.getElement(CardElement),
-                billing_details: {
-                    name: 'Jenny Rosen',
+            // step:3 --> confirm payment
+            const result = await stripe.confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: elements.getElement(CardElement),
+                    billing_details: {
+                        name: user.displayName,
+                        email: user.email,
+                    },
                 },
-            },
-        });
+            });
 
-        if(result.error) {
-            console.log(result.error.message);
-        }
-        else{
-            if(result.paymentIntent.status === 'succeeded'){
-                console.log('Payment Succeed!');
-                console.log(result);
+            if (result.error) {
+                setError(result.error.message);
+                
+            } else {
+                setError('');
+                if (result.paymentIntent.status === "succeeded") {
+                    console.log("Payment Succeed!");
+                    console.log(result);
+                    // step: 4 --> mark parcel "Paid", create payment history
+                }
             }
         }
-
 
         // console.log('response from intent: ', res);
     };
@@ -94,19 +98,16 @@ const PaymentForm = () => {
                 onSubmit={handleSubmit}
                 className="space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto"
             >
-                <CardElement className="p-2 border rounded">
-                </CardElement>
-                    <button 
+                <CardElement className="p-2 border rounded"></CardElement>
+                <button
                     type="submit"
-                    className="btn btn-primary text-black w-full" 
-                    disabled={!stripe}>
-                        Pay ${amount}
-                    </button>
+                    className="btn btn-primary text-black w-full"
+                    disabled={!stripe}
+                >
+                    Pay ${amount}
+                </button>
 
-
-                    {
-                        error && <p className="text-red-500">{error}</p>
-                    }
+                {error && <p className="text-red-500">{error}</p>}
             </form>
         </div>
     );
