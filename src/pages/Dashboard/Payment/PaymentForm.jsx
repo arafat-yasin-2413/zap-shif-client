2d4c40b3bdee2a0ce0dc51243a09de46ab2d9900
Yasin <yasinarafat2413@gmail.com>
2025-06-28 15:18:42 +0600

    66-4 Done

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,10 +1,34 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const {parcelId} = useParams();
     const [error, setError] = useState('');
+    const axiosSecure = useAxiosSecure();
+
+    // console.log(parcelId);
+
+    const { isPending, data: parcelInfo= {} } = useQuery({
+        queryKey: ['parcels', parcelId],
+        queryFn: async()=>{
+            const res = await axiosSecure.get(`/parcels/${parcelId}`);
+            return res.data;
+        }
+    })
+
+    if(isPending) {
+        return '... loading'
+    }
+
+
+    console.log(parcelInfo);
+    const amount = parcelInfo.cost;
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -45,7 +69,7 @@ const PaymentForm = () => {
                     type="submit"
                     className="btn btn-primary text-black w-full" 
                     disabled={!stripe}>
-                        Pay For Parcel Pickup
+                        Pay ${amount}
                     </button>
 
 
